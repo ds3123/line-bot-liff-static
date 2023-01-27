e2f32ea0a32e98789f61dba06fbadd01394ea4d8
DS <ds312306@gmail.com>
2023-01-27 14:28:34 +0800

    account-info

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,61 +1,13 @@
-import { useEffect , useState } from 'react';
-import { useLiff } from 'react-liff';
+import Account_Info from "component/layout/header/Account_Info"
 
 
 
 const App = () => {
 
-   const [ displayName , setDisplayName ]        = useState('') ;
-   const { error , isLoggedIn , isReady , liff } = useLiff() ;
-
-
-   // 顯示資訊
-   const showDisplayName = () => {
-       
-      // # 錯誤
-      if ( error ) return <p className="text-red-400"> 發生錯誤 </p>;
-      
-      // # 載入中 
-      if ( !isReady ) return <p className="text-blue-400"> 載入中 ... </p>;
   
-      // # 未登入
-      if ( !isLoggedIn ) {
-        
-        return <button className="bg-green-400 p-3 rounded-md" onClick = { liff.login as any }>
-                  登入 Line
-               </button> ;
-      }
-
-      // # 已登入 
-      return <>
-                  <p> 您好 : { displayName }</p>
-                  <button className="App-button" onClick = { liff.logout } >
-                     登出 Line 
-                  </button>
-             </>;
-    
-   } ;
-  
-
-   // 登入處理
-   useEffect(() => {
-
-     if ( !isLoggedIn ) return ;
- 
-     ( async () => {
-
-       const profile = await liff.getProfile() ;
-       setDisplayName( profile.displayName ) ;
-
-     })() ;
-
-   } , [ liff , isLoggedIn ] ) ;
-
-
-
    return <div>
 
-             <header> { showDisplayName() } </header>
+             <Account_Info />
              
           </div>
 
