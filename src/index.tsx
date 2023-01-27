9f0e13a56b10a816cf9248c0e316ac8342d41096
DS <ds312306@gmail.com>
2023-01-27 13:52:12 +0800

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './css/index.css';
+import App from './container/App';
 import reportWebVitals from './reportWebVitals';
+
+// LINE LIFF
+import { LiffProvider } from 'react-liff';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+// LineBot LIFF id
+const liffId = process.env.REACT_APP_LINE_LIFF_ID;
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+            <React.StrictMode>
+
+              <LiffProvider liffId = { liffId as any } >
+
+                    <App />
+
+              </LiffProvider>
+              
+            </React.StrictMode>
+          ) ;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
