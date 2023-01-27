b74b3afac4d32826709617942bad2b2844d5cf36
DS <ds312306@gmail.com>
2023-01-27 16:20:05 +0800

    test-fetch-data

--- src/container/App.tsx
+++ src/container/App.tsx
@@ -1,16 +1,47 @@
 /* eslint-disable react/jsx-pascal-case */
 import Account_Info from "component/layout/header/Account_Info"
 
+import { useState , useEffect } from 'react' ;
+
+import axios from 'axios' ;
+
 
 
 const App = () => {
+
+  const [ data , set_Data ] = useState< any[] >( [] )  ;
+
+
+  useEffect( () => {
+    
+
+    axios.get( "http://demo04.cchouse.com.tw/backend/public/index.php/api/basics" ).then( res => {
+
+        const data = res.data ;
+
+        
+        set_Data( [ ...data , res.data ] )
+
+
+
+        console.log( 'bbb' , data )
+
+
+
+    } )
+     
+     
+  } , [] ) ;
 
   
    return <div>
 
              <Account_Info />
 
-             Test
+             <hr className="m-10" />
+
+             { data[0]['basic_id'] }
+
              
           </div>
 
@@ -18,3 +49,4 @@ const App = () => {
 
 export default App 
        
+ 
